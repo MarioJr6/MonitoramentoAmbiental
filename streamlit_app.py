8a89bf684867f1a5e2154d74a4ccf45c7d8a2c74
MarioJr6 <154622586+MarioJr6@users.noreply.github.com>
2023-12-27 10:48:23 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,14 +20,34 @@ col3.image('https://github.com/MarioJr6/MonitoramentoAmbiental/blob/main/Logo%20
 
 coluna_filtro, coluna_grafico = st.columns([1,2])
 
+# Leitura dos dados
 df_esgoto = pd.read_table('https://docs.google.com/spreadsheets/d/e/2PACX-1vTZfjxdY8_x5WNd9_NE3QQPeche-dMdY5KdvNpq8H4W-lmUTidwrKpV0uLzLtihV7UAPIl68WvugMsN/pub?gid=0&single=true&output=tsv')
+df_casos = pd.read_csv('https://ti.saude.rs.gov.br/covid19/download', encoding="UTF-8", sep=";")
 
+# Formatando para o tipo data as colunas data de sintomas e confirmacao
+df_casos['DATA_SINTOMAS']=pd.to_datetime(df_casos['DATA_SINTOMAS'], format='%d/%m/%Y')
+df_casos['DATA_CONFIRMACAO']=pd.to_datetime(df_casos['DATA_CONFIRMACAO'], format='%d/%m/%Y')
+
+# Agrupando os dados para eu possuir todas as data do ano
+grouped = pd.pivot_table(data=df_casos, index='DATA_SINTOMAS', columns='MUNICIPIO', values='CRITERIO', aggfunc='count').fillna(0).reset_index()
+
+# Lista das colunas que desejo trabalhar
+colunas = ['DATA_SINTOMAS', 'CAPÃO DA CANOA', 'CAXIAS DO SUL', 'PASSO FUNDO',
+           'SANTA MARIA', 'SANTA ROSA', 'SÃO LEOPOLDO', 'TORRES']
+
+# Filtro das colunas que desejo manter e da minha linha temporal
+grouped = grouped[colunas]
+grouped = grouped[grouped['DATA_SINTOMAS']>='2023-01-01']
+
+# Lista para selectbox
 muni = ['CAPÃO DA CANOA', 'CAXIAS DO SUL', 'PASSO FUNDO','SANTA MARIA', 'SANTA ROSA', 'SÃO LEOPOLDO', 'TORRES']
 
+# Formatacao dos meus dados e filtro temporal
 df_esgoto['Data de coleta']=pd.to_datetime(df_esgoto['Data de coleta'], format='%d/%m/%Y')
-df_esgoto=df_esgoto[df_esgoto['Data de coleta']>='2023-01-01']
 df_esgoto['carga_viral_n1'] = df_esgoto['carga_viral_n1'].astype(float)
+df_esgoto=df_esgoto[df_esgoto['Data de coleta']>='2023-01-01']
 
+# Subplots para o gráfico
 fig = make_subplots(specs=[[{"secondary_y": True}]])
 
 with coluna_filtro: 
@@ -36,8 +56,13 @@ with coluna_filtro:
     filtro = df_esgoto['Município']==municipio
     df_esgoto_filtrado = df_esgoto[filtro]
 
-with coluna_grafico: 
-    fig.add_trace(
+with coluna_grafico:
+    fig = fig.add_trace(
+      go.Scatter(x=grouped['DATA_SINTOMAS'], y=grouped[municipio], name="Casos diários", mode="lines"),
+      secondary_y=True,
+  )
+
+    fig = fig.add_trace(
       go.Bar(x=df_esgoto_filtrado['Data de coleta'], y=df_esgoto_filtrado['carga_viral_n1'], name="Carga Viral no esgoto",
              ),
       secondary_y=False,
