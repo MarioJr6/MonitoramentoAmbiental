6bdfd57bf32b4e9c294cffadcc0942f5c76fe683
MarioJr6 <154622586+MarioJr6@users.noreply.github.com>
2023-12-27 10:16:41 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,3 +19,14 @@ col2.title('Painel de Monitoramento Ambiental de SARS-CoV-2')
 col3.image('https://github.com/MarioJr6/MonitoramentoAmbiental/blob/main/Logo%20Estado.png?raw=true', width=300)
 
 coluna_filtro, coluna_grafico = st.columns([1,2])
+
+df_esgoto = pd.read_table('https://docs.google.com/spreadsheets/d/e/2PACX-1vTZfjxdY8_x5WNd9_NE3QQPeche-dMdY5KdvNpq8H4W-lmUTidwrKpV0uLzLtihV7UAPIl68WvugMsN/pub?gid=0&single=true&output=tsv')
+
+with coluna_filtro: 
+    muni=st.selectbox('Selecione o município:', sorted(df_esgoto['Município'].unique()))
+
+filtro=df_esgoto['Município']==muni
+df_esgoto_filtrado=df_esgoto[filtro]
+
+with coluna_grafico: 
+    df_esgoto
