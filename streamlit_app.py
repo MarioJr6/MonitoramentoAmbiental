f971c3807b19a1d318b4fab2c5e6bb6997d4348b
MarioJr6 <154622586+MarioJr6@users.noreply.github.com>
2023-12-27 10:30:40 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,4 +23,4 @@ coluna_filtro, coluna_grafico = st.columns([1,2])
 df_esgoto = pd.read_table('https://docs.google.com/spreadsheets/d/e/2PACX-1vTZfjxdY8_x5WNd9_NE3QQPeche-dMdY5KdvNpq8H4W-lmUTidwrKpV0uLzLtihV7UAPIl68WvugMsN/pub?gid=0&single=true&output=tsv')
 
 with coluna_filtro: 
-   municipio = st.selectbox('Selecione o município', sorted(df_esgoto['Município'].unique()))
+   municipio = st.selectbox('Selecione o município')
