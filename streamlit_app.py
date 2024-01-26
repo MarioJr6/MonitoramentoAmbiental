9413d7aae34d0f4b93ce403a5ee7930569f3147a
MarioJr6 <154622586+MarioJr6@users.noreply.github.com>
2024-01-26 12:45:46 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,14 +11,23 @@ from streamlit_extras.metric_cards import style_metric_cards
 
 @st.cache_data
 def fetch_and_clean_data(url):
+  df_casos = pd.read_csv(url, encoding="UTF-8", sep=";")
+  df_casos['DATA_SINTOMAS']=pd.to_datetime(df_casos['DATA_SINTOMAS'], format='%d/%m/%Y')
+  df_casos['DATA_CONFIRMACAO']=pd.to_datetime(df_casos['DATA_CONFIRMACAO'], format='%d/%m/%Y')
 
-  
-  
-    return data
+  # Agrupando os dados de forma que eu tenha todas as datas do ano
+  grouped = pd.pivot_table(data=df_casos, index='DATA_SINTOMAS', columns='MUNICIPIO', values='CRITERIO', aggfunc='count').fillna(0).reset_index()
 
-d1 = fetch_and_clean_data(DATA_URL_1)
+  colunas = ['DATA_SINTOMAS', 'CAPÃO DA CANOA', 'CAXIAS DO SUL', 'PASSO FUNDO',
+           'SANTA MARIA', 'SANTA ROSA', 'TORRES']
+  grouped = grouped[colunas]
+
+    return grouped
+
+d1 = fetch_and_clean_data('https://ti.saude.rs.gov.br/covid19/download?2023')
+
+d1 
 # Actually executes the function, since this is the first time it was
 # encountered.
 
-d2 = fetch_and_clean_data(DATA_URL_1)
 
