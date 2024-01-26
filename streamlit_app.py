d28262d7724dfd056d40b1f92df62dc5181ae8ba
MarioJr6 <154622586+MarioJr6@users.noreply.github.com>
2024-01-26 12:39:31 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,10 @@
+import streamlit as st
+import pandas as pd
+import requests
+import plotly.express as px
+import plotly.graph_objects as go
+import streamlit_extras
+import numpy as np
 
+from plotly.subplots import make_subplots
+from streamlit_extras.metric_cards import style_metric_cards
