6ee2f464d9cae671e8793a0e7bdd5d1498f923b8
MarioJr6 <154622586+MarioJr6@users.noreply.github.com>
2024-01-26 12:42:13 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,17 @@ import numpy as np
 
 from plotly.subplots import make_subplots
 from streamlit_extras.metric_cards import style_metric_cards
+
+@st.cache_data
+def fetch_and_clean_data(url):
+
+  
+  
+    return data
+
+d1 = fetch_and_clean_data(DATA_URL_1)
+# Actually executes the function, since this is the first time it was
+# encountered.
+
+d2 = fetch_and_clean_data(DATA_URL_1)
+
